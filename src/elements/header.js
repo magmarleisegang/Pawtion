614f72d676ded5ef3f9fe42cbdd7106cd909fb76
magdalena.leisegang <magdalena.leisegang@entelect.co.za>
2021-03-28 21:50:02 +0200

    fix the header styling (a bit)

--- src/elements/header.js
+++ src/elements/header.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 import {
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
     Text,
-    useColorScheme,
     View,
-    Image
+    StyleSheet
 } from 'react-native';
-import logo from './paws.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faPaw } from '@fortawesome/free-solid-svg-icons'
+// import logo from './paws.svg';
+
+const headerStyles = StyleSheet.create({
+    container: {
+        backgroundColor: '#285190',
+        color: 'white',
+        width: '100%',
+        height: 50,
+        fontSize: 20,
+        textAlignVertical: 'center',
+        padding: 10
+    },
+    logo: {
+        width: 25,
+        height: 25,
+        color: 'white'
+    }
+});
 
 export default class Header extends Component {
     render() {
         return (
             <View>
-                <Image source={logo} />
-                <Text>Pawtion</Text>
+                <Text style={[headerStyles.container]}>
+                    <FontAwesomeIcon icon={faPaw} style={[headerStyles.logo]} />{' '}
+                Welcome to Pawtion</Text>
             </View>
         )
 
